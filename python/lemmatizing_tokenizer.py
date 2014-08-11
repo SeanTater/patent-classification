cee0cfb84a4af16b0f38831a0b7d94ab5b80651c
walid-shalaby <wshalaby@uncc.edu>
2014-08-11 08:46:20 -0400

    initial commit

---
+++ python/lemmatizing_tokenizer.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+## Tokenizer with Lemmatizer
+
+####### A tokenizer that lemmatize tokenized documents text using nltk wordnet lemmatizer
+
+# In[1]:
+
+from nltk import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.tokenizer = RegexpTokenizer(u'(?u)\\b[a-z]+\\-*[a-z]+|\\b(?u)\\b[a-z]\\b')
+        self.wnl = WordNetLemmatizer()
+    def __call__(self, doc):
+        return [self.wnl.lemmatize(t) for t in self.tokenizer.tokenize(doc)]
+
+
+# In[ ]:
+
+
+
---
+++ python/stemming_tokenizer.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+## Tokenizer with Stemmer
+
+####### A tokenizer that stem tokenized documents text using nltk porter stemmer
+
+# In[1]:
+
+from nltk.stem import PorterStemmer
+from nltk import RegexpTokenizer
+class StemmingTokenizer(object):
+    def __init__(self):
+        self.stemmer = PorterStemmer()
+        self.tokenizer = RegexpTokenizer(u'(?u)\\b[a-z]+\\-*[a-z]+|\\b(?u)\\b[a-z]\\b')
+    def __call__(self, doc):
+        return [self.stemmer.stem(tokens.lower()) for tokens in self.tokenizer.tokenize(doc)]
+
+
+# In[ ]:
+
+
+
